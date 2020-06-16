38282432dbff25828759a337329437a3835b2eb4
Ravi Bhankharia <ravibanks54@gmail.com>
2020-06-15 21:54:19 -0700

    Initial Commit

---
+++ app/src/main/java/com/aware/androidupdatetemplateapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.aware.androidupdatetemplateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    public void checkForUpdates(View view) {
+        final TextView textView = (TextView) findViewById(R.id.text);
+        String localhost = "http://10.0.2.2:5000/";
+        JsonObjectRequest stringRequest =
+                new JsonObjectRequest(Request.Method.GET, localhost, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                textView.setText(response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
+        queue.add(stringRequest);
+    }
+}
